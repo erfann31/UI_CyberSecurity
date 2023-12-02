0d46044addb17fc6d0390a656ad3542ac6b09a80
erfann31 <erfannasri2@gmail.com>
2023-12-03 00:45:16 +0330

    implement attacks

--- Vigenère/main.py
+++ Vigenère/main.py
@@ -26,11 +26,27 @@ def vigenere_decrypt(ciphertext, key):
     return decrypted_text
 
 
-# Kasiski Examination for finding key length
-def kasiski_examination(ciphertext):
+# Frequency Analysis Attack
+def frequency_analysis(ciphertext):
+    letter_frequencies = {}
+    for letter in ciphertext:
+        if letter.isalpha():
+            if letter in letter_frequencies:
+                letter_frequencies[letter] += 1
+            else:
+                letter_frequencies[letter] = 1
+
+    # Sort frequencies
+    sorted_frequencies = sorted(letter_frequencies.items(), key=lambda x: x[1], reverse=True)
+
+    return sorted_frequencies
+
+
+# Kasiski Examination
+def kasiski_examination(ciphertext, key_length):
     repeated_sequences = {}
-    for i in range(len(ciphertext) - 3):
-        sequence = ciphertext[i:i + 3]
+    for i in range(len(ciphertext) - key_length):
+        sequence = ciphertext[i:i + key_length]
         if sequence in repeated_sequences:
             repeated_sequences[sequence].append(i)
         else:
@@ -50,16 +66,38 @@ def kasiski_examination(ciphertext):
     return distances
 
 
-# Function to determine probable key length
+# Known Plaintext Attack
+def known_plaintext_attack(plaintext, ciphertext):
+    possible_keys = []
+    for i in range(len(plaintext)):
+        key_char = chr(((ord(ciphertext[i]) - ord(plaintext[i])) % 26) + 65)
+        possible_keys.append(key_char)
+
+    return ''.join(possible_keys)
 
 
 # Example usage
-plaintext = "CRYPTO IS SHORT FOR CRYPTOGRAPHY. "
-key = "KEY"
+plaintext = "CRYPTO IS SHORT FOR CRYPTOGRAPHY".upper()
+key = "ABCD"
 encrypted_text = vigenere_encrypt(plaintext, key)
 print("Plain Text:", plaintext)
 print("Encrypted Text:", encrypted_text)
 decrypted_text = vigenere_decrypt(encrypted_text, key)
 print("Decrypted Text:", decrypted_text)
 
+# Test the attacks
 
+# Frequency Analysis Attack
+freq_analysis_result = frequency_analysis(encrypted_text)
+print("\nFrequency Analysis Result:")
+print(freq_analysis_result)
+
+# Kasiski Examination
+kasiski_result = kasiski_examination(encrypted_text, 4)
+print("\nKasiski Examination Result:")
+print(kasiski_result)
+
+# Known Plaintext Attack
+known_plaintext_result = known_plaintext_attack(plaintext, encrypted_text)
+print("\nKnown Plaintext Attack Result:")
+print("Probable key: ",known_plaintext_result)
